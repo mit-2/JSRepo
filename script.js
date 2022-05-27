fdeb31a0225a676fa8819dc0e5d496a5ee194b2d
Ilya Mityagin <mit2@mail.ru>
2022-05-27 23:11:36 +0300

    start project

--- script.js
+++
@@ -1,9 +0,0 @@
-alert("Hello my script");
-console.log("Hello console");
-
-const div = document.getElementById('github');
-const logged = () => {
-    console.log(div.textContent);
-};
-
-console.log("master");
