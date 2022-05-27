a234d698ed0a509bbb546b4e333d69268627c4f9
Ilya Mityagin <mit2@mail.ru>
2022-05-28 01:13:37 +0300

    добавлены перемнные, alert, console.log

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log ('Hello, world!');
+let title = 1;
+let screens = 2;
+let screenPrice = 3;
+let rollback = 4;
+let fullPrice = 5;
+let adaptiv = 6;
+
+alert("Hello, world");
+
+console.log ("Hello, console!");
